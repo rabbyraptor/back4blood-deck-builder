b1665d3f189b00dc3e6dee41150d60e237fa1de8
Mathias Hammer <rabbyraptor@hotmail.com>
2021-11-03 21:38:24 +0100

    Add background image

--- src/App.vue
+++ src/App.vue
@@ -27,18 +27,36 @@ export default {
 
 html {
   font-size: 10px;
+  height: 100%;
+  width: 100%;
+  display: flex;
 }
 
 body {
+  display: flex;
+  height: 100%;
+  width: 100%;
   background: #000023;
+  background-image: url("/images/b4b_titlescreen_bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
   user-select: none;
+  background-attachment: fixed;
 }
 
 header {
-  margin: 4rem 0 1.9rem;
-  text-align: center;
+  margin: 0;
+  padding-right: 6rem;
+  height: 5rem;
   color: white;
-  font-size: 3rem;
+  font-size: 1rem;
+  position: absolute;
+  display: flex;
+  flex-grow: 1;
+  width: 100%;
+  align-items: center;
+  justify-content: flex-end;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
