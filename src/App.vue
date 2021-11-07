4d85a55d1a2ddd236b4a0dc383b462a0648ad047
Mathias Hammer <rabbyraptor@hotmail.com>
2021-11-07 23:13:41 +0100

    Add kofi button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <header>
-      <h3>B4B Deck Builder</h3>
+      <h3>B4B Deck Builder v1.0</h3>
+      <a href="https://ko-fi.com/B0B36WCPF" target="_blank"
+        ><img
+          height="20"
+          class="kofi-button"
+          src="https://cdn.ko-fi.com/cdn/kofi2.png?v=3"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+      /></a>
     </header>
     <router-view />
   </div>
@@ -58,17 +66,24 @@ header {
   width: 100%;
   height: 5rem;
   margin: 0;
-  padding-right: 6rem;
+  padding: 0 5rem;
   color: white;
   font-size: 1rem;
   z-index: 1;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   background-color: #000;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.kofi-button {
+  border: 0px;
+  height: 2.5rem;
+  margin-top: 0.4rem;
+  margin-left: 2rem;
 }
 </style>
